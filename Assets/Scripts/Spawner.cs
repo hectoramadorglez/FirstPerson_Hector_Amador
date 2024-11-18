dcaade3150023747e5bbe68b6c4dfa4a03ed150f
hectoramadorglez <hectoramador2005@gmail.com>
2024-11-18 20:30:49 +0100

    sgadg

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -8,11 +8,15 @@ public class Spawner : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] private GameObject enemigoPrefab;
     [SerializeField] private Transform[] puntoSpawn;
+    [SerializeField] private int numeroNiveles;
+    [SerializeField] private int esperarEntreSpawns;
+    [SerializeField] private int rondasPorNivel;
+    [SerializeField] private int spawnsPorRondas;
     void Start()
     {
 
         Instantiate(enemigoPrefab, puntoSpawn[Random.Range(0,puntoSpawn.Length)].position, Quaternion.identity);
-        StartCoroutine(PuntosDeSpawn());
+        StartCoroutine(Spawnear());
     }
 
     // Update is called once per frame
@@ -20,15 +24,27 @@ public class Spawner : MonoBehaviour
     {
         
     }
-    private IEnumerator  PuntosDeSpawn()
+    private IEnumerator  Spawnear()
     {
+        for (int i = 0; i < numeroNiveles;i++) 
+        {
+            for (int j = 0; j < rondasPorNivel; j++)
+            {
+                for (int k = 0; k < spawnsPorRondas; k++)
+                {
+                    int indiceAleatorio = Random.Range(0,puntoSpawn.Length);
+                    Instantiate(enemigoPrefab, puntoSpawn[indiceAleatorio].position, Quaternion.identity);
+                    yield return new WaitForSeconds(esperarEntreSpawns);
 
-        while (true) 
-        { 
-            Instantiate
-        
-        
+                }
+                yield return new WaitForSeconds(esperarEntreSpawns);
+            }
+            yield return new WaitForSeconds(esperarEntreSpawns);
+
+
+
         }
-    
+        
     }
+
 }
