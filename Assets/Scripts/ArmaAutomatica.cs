47fe9349d55806f39fc1fbca394029502c937243
hectoramadorglez <hectoramador2005@gmail.com>
2024-11-27 18:25:34 +0100

    jjj

--- Assets/Scripts/ArmaAutomatica.cs
+++ Assets/Scripts/ArmaAutomatica.cs
@@ -5,14 +5,37 @@ using UnityEngine;
 public class ArmaAutomatica : MonoBehaviour
 {
     // Start is called before the first frame update
+    private Camera cam;
+    [SerializeField] private ArmaSo misDatos;
     void Start()
     {
+        cam = Camera.main;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            System.Play();
+            if (Physics.Raycast(cam.transform.position, cam.transform.forward, out RaycastHit hitInfo, misDatos)) 
+            {
+                if (hitInfo.transform.CompareTag("")) 
+                { 
+                
+                
+                
+                }
+            
+            }
+
+
+        }
     }
+    
+
+
+
+
 }
